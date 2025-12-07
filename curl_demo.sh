8ab3c68fb14578aa35879745061372b820777fb8
drkwitht <derektan3510@gmail.com>
2025-12-07 10:17:09 -0800

    Added keep-alive to curl demo.

--- curl_demo.sh
+++ curl_demo.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-curl -v http://localhost:8080/ -H "Content-Type: */*" -H "Content-Length: 0" -H "Connection: close" || echo "Request failed, check server logs.";
+curl -i http://localhost:8080/ -H "Content-Type: */*" -H "Content-Length: 0" -H "Connection: keep-alive" && curl -i http://localhost:8080/foo -H "Content-Type: */*" -H "Content-Length: 0" -H "Connection: close" || echo "\033[1;32mDemo is DONE\033[0m";
