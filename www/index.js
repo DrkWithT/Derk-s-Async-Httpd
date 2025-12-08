328f4b4e9cc8642c46c34e5915a167b9a97594db
drkwitht <derektan3510@gmail.com>
2025-12-07 21:54:39 -0800

    Revamped www assets.

--- www/index.js
+++ www/index.js
@@ -1,22 +1,16 @@
-(function (doc) {
-    const userText = doc.querySelector('textarea');
+((doc) => {
+    const userText = doc.querySelector('input#usertxt');
     const submitButton = doc.querySelector('button');
-
     submitButton.addEventListener('click', async () => {
         let response = await fetch('/', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'text/plain'
-            },
+            headers: {'Content-Type': 'text/plain'},
             body: `${userText.innerText}`
         }).catch(err => console.error($`AJAX Error: ${err}`)) || null;
 
         if (!response) {
             console.error('AJAX Error: failed to get any response.');
-            return;
-        }
-
-        if (response.ok) {
+        } else if (response.ok) {
             userText.innerText = await response.text();
         } else {
             console.error(`AJAX Error: found a bad response code of ${response.status}`);
