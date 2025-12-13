3edff2b25a0322a1c6be5941b52b7c25ef16000c
drkwitht <derektan3510@gmail.com>
2025-12-12 22:11:19 -0800

    Added Host header validation.

--- include/myapp/routes.hpp
+++ include/myapp/routes.hpp
@@ -9,6 +9,7 @@
 #include "myhttp/msgs.hpp"
 #include "myuri/uri.hpp"
 #include "myuri/parse.hpp"
+#include "myapp/response_helpers.hpp"
 
 namespace DerkHttpd::App {
     /// NOTE: Provides an alias for any callable entity that generates a web response given a path and some parameters.
@@ -18,38 +19,42 @@ namespace DerkHttpd::App {
     private:
         Middleware m_fallback;
         std::map<std::string, Middleware> m_handlers;
+        std::string m_host_str;
+
+        /// NOTE: checks if the request has a valid "Host" (if HTTP/1.1)
+        [[nodiscard]] auto check_host_header(const Http::Request& req) const -> bool;
 
     public:
-        explicit Routes();
+        explicit Routes(std::string server_host);
 
         [[maybe_unused]] auto set_handler(const std::string& route_path, Middleware handler_box) noexcept -> bool;
 
         template <typename Req> requires (std::is_same_v<std::remove_cvref_t<Req>, Http::Request>)
         [[nodiscard]] auto dispatch_handler(Req&& req) const noexcept -> Http::Response {
-            std::string handler_err_msg {"Check server log."};
-            auto is_error_not_found = false;
+            auto req_uri = Uri::parse_simple_uri(req.uri);
 
-            // 1. Try parsing the URI, but any error next would require a fitting HTTP 404 or 500 reply...
-            if (auto req_uri = Uri::parse_simple_uri(req.uri); req_uri.has_value()) {
-                const auto& uri_obj = req_uri.value();
+            // 1. Validate request syntax & semantics to prevent false-positive responses.
+            if (!check_host_header(req) || !req_uri) {
+                // Protocol syntax errors: `Host` header is missing or URI is malformed. 
+                Http::Response res;
+                App::EmptyReply uri_syntax_error {Http::Status::http_bad_request};
 
-                // 2. Try finding the exactly matching route handler by URI path...
-                if (auto handler_it = std::find_if(m_handlers.begin(), m_handlers.end(), [&uri_obj](const auto& route_handler_item) -> bool {
-                    return route_handler_item.first == uri_obj.path();
-                }); handler_it != m_handlers.end()) {
-                    return handler_it->second(std::forward<Req>(req), uri_obj.params());
-                }
+                App::ResponseUtils::response_put_all(res, uri_syntax_error);
 
-                is_error_not_found = true;
-            } else {
-                handler_err_msg = std::exchange(req_uri.error(), {});
+                return res;
             }
 
-            // 3. Call the default error handler on invalid URI or unknown handler. This handler MUST check is_not_found to give an HTTP-404 if appropriate.
-            return m_fallback(req, {
-                {"is_not_found", {is_error_not_found ? 1 : 0}},
-                {"err_msg", handler_err_msg},
-            });
+            const auto& uri_obj = req_uri.value();
+
+            // 2. Try finding the matching route handler by URI path.
+            if (auto handler_it = std::find_if(m_handlers.begin(), m_handlers.end(), [&uri_obj](const auto& route_handler_item) -> bool {
+                return route_handler_item.first == uri_obj.path();
+            }); handler_it != m_handlers.end()) {
+                return handler_it->second(std::forward<Req>(req), uri_obj.params());
+            }
+
+            // 3. Call the default error handler on an unset middleware route. It gives a simple 404 response for now.
+            return m_fallback(req, {});
         }
     };
 }
--- src/myapp/routes.cpp
+++ src/myapp/routes.cpp
@@ -1,40 +1,30 @@
 #include <utility>
-#include <variant>
 
 #include "myapp/routes.hpp"
+#include "myapp/response_helpers.hpp"
 
 namespace DerkHttpd::App {
     const auto dud_fallback_handler = []([[maybe_unused]] Http::Request req, const std::map<std::string, Uri::QueryValue>& params) -> Http::Response {
         Http::Response res;
-
-        if (auto error_is_not_found_p = std::get_if<int>(&params.at("is_not_found")); error_is_not_found_p) {
-            res.http_status = (*error_is_not_found_p != 0)
-                ? Http::Status::http_not_found
-                : Http::Status::http_server_error;
-        }
-
-        if (auto handle_error_msg_p = std::get_if<std::string>(&params.at("err_msg")); handle_error_msg_p) {
-            Http::Blob temp_msg_data;
-            temp_msg_data.append_range(*handle_error_msg_p);
-
-            const auto msg_length = handle_error_msg_p->length();
-
-            res.headers.emplace("Content-Type", "text/plain");
-            res.headers.emplace("Content-Length", std::to_string(msg_length));
-
-            res.body = std::move(temp_msg_data);
-        } else {
-            res.headers.emplace("Content-Type", "*/*");
-            res.headers.emplace("Content-Length", "0");
-
-            res.body = {};
-        }
+        
+        App::StringReply generic_error_msg {"No matching middleware found.", "text/plain"};
+        App::ResponseUtils::response_put_all(res, generic_error_msg, Http::Status::http_not_found);
 
         return res;
     };
 
-    Routes::Routes()
-    : m_fallback {dud_fallback_handler}, m_handlers {} {}
+    auto Routes::check_host_header(const Http::Request& req) const -> bool {
+        if (req.http_schema == Http::Schema::http_1_1 && !req.headers.contains("Host")) {
+            return false;
+        } else if (const auto& host_value = req.headers.at("Host"); host_value != m_host_str) {
+            return false;
+        }
+
+        return true;
+    }
+
+    Routes::Routes(std::string server_host)
+    : m_fallback {dud_fallback_handler}, m_handlers {}, m_host_str {std::move(server_host)} {}
 
     auto Routes::set_handler(const std::string& route_path, Middleware handler_box) noexcept -> bool {
         if (m_handlers.contains(route_path)) {
